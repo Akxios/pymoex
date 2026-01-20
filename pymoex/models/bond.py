b0962632659f9e65203195c7a731e6dbf41deffe
Akxios <artemermolaev466@gmail.com>
2026-01-20 19:34:02 +0900

    Rename fields bond.py

--- pymoex/models/bond.py
+++ pymoex/models/bond.py
@@ -14,48 +14,48 @@ class Bond(BaseModel):
     shortname: str
     secname: Optional[str] = None
     isin: Optional[str] = None
-    regnumber: Optional[str] = None
+    reg_number: Optional[str] = None
 
     # Цена и доходность
     last_price: Optional[float] = None
-    prevwaprice: Optional[float] = None
-    prevprice: Optional[float] = None
-    prevlegalcloseprice: Optional[float] = None
-    yieldatprevwaprice: Optional[float] = None
+    prev_waprice: Optional[float] = None
+    prev_price: Optional[float] = None
+    prev_legal_close_price: Optional[float] = None
+    yiel_dat_prevwa_price: Optional[float] = None
     yield_percent: Optional[float] = None
 
     # Купоны
-    couponvalue: Optional[float] = None
-    couponpercent: Optional[float] = None
-    accruedint: Optional[float] = None
-    nextcoupon: Optional[date] = None
+    coupon_value: Optional[float] = None
+    coupon_percent: Optional[float] = None
+    accrued_int: Optional[float] = None
+    next_coupon: Optional[date] = None
 
     # Срок обращения
-    matdate: Optional[date] = None
-    couponperiod: Optional[int] = None
-    dateyieldfromissuer: Optional[date] = None
+    mat_date: Optional[date] = None
+    coupon_period: Optional[int] = None
+    date_yield_from_issuer: Optional[date] = None
 
     # Номинал и лоты
-    facevalue: Optional[float] = None
-    lotsize: Optional[int] = None
-    lotvalue: Optional[float] = None
-    faceunit: Optional[str] = None
-    currencyid: Optional[str] = None
+    face_value: Optional[float] = None
+    lot_size: Optional[int] = None
+    lot_value: Optional[float] = None
+    face_unit: Optional[str] = None
+    currency_id: Optional[str] = None
 
     # Ликвидность и листинг
-    issuesizeplaced: Optional[int] = None
-    listlevel: Optional[int] = None
+    issue_size_placed: Optional[int] = None
+    list_level: Optional[int] = None
     status: Optional[str] = None
-    sectype: Optional[str] = None
+    sec_type: Optional[str] = None
 
     # Опции (оферты, выкуп)
-    offerdate: Optional[date] = None
-    calloptiondate: Optional[date] = None
-    putoptiondate: Optional[date] = None
-    buybackdate: Optional[date] = None
-    buybackprice: Optional[float] = None
+    offer_date: Optional[date] = None
+    calloption_date: Optional[date] = None
+    put_option_date: Optional[date] = None
+    buyback_date: Optional[date] = None
+    buyback_price: Optional[float] = None
 
     # Классификация
-    bondtype: Optional[str] = None
-    bondsubtype: Optional[str] = None
-    sectorid: Optional[str] = None
+    bond_type: Optional[str] = None
+    bond_subtype: Optional[str] = None
+    sector_id: Optional[str] = None
