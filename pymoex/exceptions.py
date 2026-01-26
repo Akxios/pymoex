32dae9a020eb52b8ee27b29a70644f38066d23d8
Akxios <artemermolaev466@gmail.com>
2026-01-26 20:23:58 +0900

    Added exceptions

---
+++ pymoex/exceptions.py
@@ -0,0 +1,23 @@
+class MoexError(Exception):
+    """
+    Базовое исключение для всех ошибок SDK pymoex.
+    От него наследуются все остальные ошибки.
+    """
+
+    pass
+
+
+class InstrumentNotFoundError(MoexError):
+    """
+    Акция или облигация не найдена на Московской бирже.
+    """
+
+    pass
+
+
+class MoexAPIError(MoexError):
+    """
+    Ошибка ответа ISS API (500, битый JSON, неожиданный формат).
+    """
+
+    pass
