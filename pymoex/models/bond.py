5776cc7f274654854b0e611eed4347a67ef65b08
Akxios <artemermolaev466@gmail.com>
2026-02-01 18:47:21 +0900

    Update: fields

--- pymoex/models/bond.py
+++ pymoex/models/bond.py
@@ -39,6 +39,27 @@ class Bond(BaseModel):
     def __str__(self) -> str:
         return self.__repr__()
 
+    @property
+    def last_price(self) -> Optional[float]:
+        """
+        Последняя цена облигации без НКД (clean price) в валюте номинала.
+        """
+        if self.price_percent is None or self.face_value is None:
+            return None
+
+        return self.face_value * self.price_percent / 100
+
+    @property
+    def last_dirty_price(self) -> Optional[float]:
+        """
+        Последняя полная цена облигации (с НКД).
+        """
+        clean = self.last_price
+        if clean is None:
+            return None
+
+        return clean + (self.accruedint or 0)
+
     # --- Идентификация инструмента ---
     sec_id: str  # торговый код / ISIN
     short_name: str  # краткое название
@@ -47,13 +68,13 @@ class Bond(BaseModel):
     reg_number: Optional[str] = None
 
     # --- Рыночные показатели ---
-    last_price: Optional[float] = None  # последняя цена
+    price_percent: Optional[float] = None  # последняя цена, %
     yield_percent: Optional[float] = None  # эффективная доходность, %
 
     # --- Купонные параметры ---
     coupon_value: Optional[float] = None
     coupon_percent: Optional[float] = None
-    accrued_int: Optional[float] = None  # НКД
+    accruedint: Optional[float] = None  # НКД
     next_coupon: Optional[date] = None  # дата следующего купона
 
     # --- Сроки обращения ---
