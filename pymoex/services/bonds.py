fca6e09cf858f2f83751bed76b687c795fb18ac4
Akxios <artemermolaev466@gmail.com>
2026-02-03 19:58:21 +0900

    Update: clear code, mini fix service

--- pymoex/services/bonds.py
+++ pymoex/services/bonds.py
@@ -2,7 +2,6 @@ from pymoex.core import endpoints
 from pymoex.exceptions import InstrumentNotFoundError
 from pymoex.models.bond import Bond
 from pymoex.utils.table import first_row
-from pymoex.utils.types import safe_date
 
 
 class BondsService:
@@ -49,51 +48,20 @@ class BondsService:
 
         if not market_rows:
             raise InstrumentNotFoundError(f"Bond {ticker} has no market data")
+
         sec = self._pick_trading_row(market_rows)
         yld = first_row(market.get("marketdata_yields"))
-        yield_percent = self._extract_yield(yld)
+        effective_yield = yld.get("EFFECTIVEYIELD") if yld else None
 
-        return Bond(
-            # Идентификация
-            sec_id=sec["SECID"],
-            short_name=sec["SHORTNAME"],
-            sec_name=sec.get("SECNAME"),
-            is_in=sec.get("ISIN"),
-            reg_number=sec.get("REGNUMBER"),
-            # Цена и доходность
-            price_percent=self._extract_price_percent(sec),
-            yield_percent=yield_percent,
-            # Купоны
-            coupon_value=sec.get("COUPONVALUE"),
-            coupon_percent=sec.get("COUPONPERCENT"),
-            accruedint=sec.get("ACCRUEDINT"),
-            next_coupon=safe_date(sec.get("NEXTCOUPON")),
-            # Сроки
-            mat_date=safe_date(sec.get("MATDATE")),
-            coupon_period=sec.get("COUPONPERIOD"),
-            date_yield_from_issuer=safe_date(sec.get("DATEYIELDFROMISSUER")),
-            # Номинал и лоты
-            face_value=sec.get("FACEVALUE"),
-            lot_size=sec.get("LOTSIZE"),
-            lot_value=sec.get("LOTVALUE"),
-            face_unit=sec.get("FACEUNIT"),
-            currency_id=sec.get("CURRENCYID"),
-            # Ликвидность и листинг
-            issue_size_placed=sec.get("ISSUESIZEPLACED"),
-            list_level=sec.get("LISTLEVEL"),
-            status=sec.get("STATUS"),
-            sec_type=sec.get("SECTYPE"),
-            # Опции
-            offer_date=safe_date(sec.get("OFFERDATE")),
-            calloption_date=safe_date(sec.get("CALLOPTIONDATE")),
-            put_option_date=safe_date(sec.get("PUTOPTIONDATE")),
-            buyback_date=safe_date(sec.get("BUYBACKDATE")),
-            buyback_price=sec.get("BUYBACKPRICE"),
-            # Классификация
-            bond_type=sec.get("BONDTYPE"),
-            bond_sub_type=sec.get("BONDSUBTYPE"),
-            sector_id=sec.get("SECTORID"),
-        )
+        price_percent = self._extract_price_percent(sec)
+
+        data = {
+            **sec,
+            "price_percent": price_percent,
+            "effective_yield": effective_yield,
+        }
+
+        return Bond.model_validate(data)
 
     def _pick_trading_row(self, rows: list[dict]) -> dict:
         """
@@ -112,9 +80,3 @@ class BondsService:
         Извлечение цены (% от номинала) с fallback.
         """
         return sec.get("PREVWAPRICE") or sec.get("PREVPRICE") or sec.get("MARKETPRICE")
-
-    @staticmethod
-    def _extract_yield(yld: dict | None) -> float | None:
-        if not yld:
-            return None
-        return yld.get("EFFECTIVEYIELD")
