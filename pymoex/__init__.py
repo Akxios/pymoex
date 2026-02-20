9ea482dfabc3db1b8c3e1082fa002dcd497416a1
Akxios <artemermolaev466@gmail.com>
2026-02-20 20:10:16 +0900

    Update: deleted field level logging

--- pymoex/__init__.py
+++ pymoex/__init__.py
@@ -1,27 +1,15 @@
 import logging
 
-from pymoex.client import MoexClient
-from pymoex.core.config import MoexSettings
-from pymoex.models.enums import InstrumentType
-
 from .api import find, find_bonds, find_shares, get_bond, get_share
+from .client import MoexClient
 
-logger = logging.getLogger(__name__)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-try:
-    settings = MoexSettings()
-    log_level = getattr(logging, settings.log_level.upper(), logging.INFO)
-    logger.setLevel(log_level)
-except Exception:
-    logger.setLevel(logging.INFO)
 
 __all__ = [
     "MoexClient",
-    "InstrumentType",
     "get_share",
     "get_bond",
+    "find",
     "find_shares",
     "find_bonds",
-    "find",
 ]
