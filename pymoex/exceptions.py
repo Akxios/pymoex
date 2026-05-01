362899e5a8d766dfaa801a72e0b196788ec0e0c1
Axkios <artemermolaev466@gmail.com>
2026-05-01 20:37:28 +0900

    Update: added new exceptions

--- pymoex/exceptions.py
+++ pymoex/exceptions.py
@@ -1,29 +1,50 @@
 class MoexError(Exception):
-    """
-    Базовое исключение для всех ошибок SDK pymoex.
-    От него наследуются все остальные ошибки.
-    """
-
     pass
+    """Базовое исключение для всех ошибок SDK pymoex."""
 
 
 class InstrumentNotFoundError(MoexError):
-    """
-    Акция или облигация не найдена на Московской бирже.
-    """
-
     pass
+    """Акция или облигация не найдена на Московской бирже."""
 
 
 class MoexAPIError(MoexError):
-    """
-    Ошибка ответа ISS API (500, битый JSON, неожиданный формат).
-    """
-
     pass
+    """Базовое исключение для ошибок ISS API."""
 
 
 class MoexNetworkError(MoexError):
-    """Ошибки сети или HTTP статусы 4xx/5xx"""
-
     pass
+    """Базовое исключение для сетевых и HTTP ошибок."""
+
+
+class MoexHTTPError(MoexNetworkError):
+    """Базовое исключение для HTTP статусов 4xx/5xx."""
+
+
+class MoexBadRequestError(MoexHTTPError):
+    """Некорректный запрос к API (HTTP 400)."""
+
+
+class MoexAuthError(MoexHTTPError):
+    """Ошибка авторизации/аутентификации (HTTP 401/403)."""
+
+
+class MoexNotFoundError(MoexHTTPError):
+    """Ресурс не найден (HTTP 404)."""
+
+
+class MoexRateLimitError(MoexHTTPError):
+    """Превышен лимит запросов (HTTP 429)."""
+
+
+class MoexServerError(MoexHTTPError):
+    """Ошибка на стороне MOEX API (HTTP 5xx)."""
+
+
+class MoexTimeoutError(MoexNetworkError):
+    """Таймаут сетевого запроса к MOEX API."""
+
+
+class MoexResponseParseError(MoexAPIError):
+    """Ошибка декодирования JSON или неожиданный формат ответа."""
