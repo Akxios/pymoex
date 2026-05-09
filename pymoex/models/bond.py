bbe545f94c71df09d66823572008acd4f57ab7a0
Axkios <artemermolaev466@gmail.com>
2026-05-09 20:27:53 +0900

    Update: clean bond.py

--- pymoex/models/bond.py
+++ pymoex/models/bond.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from typing import override
 
 from pydantic import Field, computed_field, model_validator
 
@@ -25,182 +26,193 @@ class Bond(BaseInstrument):
     sec_id: str = Field(alias="SECID")
     """Идентификатор финансового инструмента"""
 
-    board_id: str | None = Field(None, alias="BOARDID")
+    board_id: str | None = Field(default=None, alias="BOARDID")
     """Идентификатор режима торгов"""
 
-    isin: str | None = Field(None, alias="ISIN")
+    isin: str | None = Field(default=None, alias="ISIN")
     """ISIN"""
 
     short_name: str = Field(alias="SHORTNAME")
     """Краткое наименование ценной бумаги"""
 
-    name: str | None = Field(None, alias="SECNAME")
+    name: str | None = Field(default=None, alias="SECNAME")
     """Наименование финансового инструмента"""
 
-    reg_number: str | None = Field(None, alias="REGNUMBER")
+    reg_number: str | None = Field(default=None, alias="REGNUMBER")
     """Регистрационный номер"""
 
-    status: str | None = Field(None, alias="STATUS")
+    status: str | None = Field(default=None, alias="STATUS")
     """Статус"""
 
-    list_level: MoexInt = Field(None, alias="LISTLEVEL")
+    list_level: MoexInt = Field(default=None, alias="LISTLEVEL")
     """Уровень листинга"""
 
-    sec_type: str | None = Field(None, alias="SECTYPE")
+    sec_type: str | None = Field(default=None, alias="SECTYPE")
     """Тип ценной бумаги"""
 
-    bond_type: str | None = Field(None, alias="BONDTYPE")
+    bond_type: str | None = Field(default=None, alias="BONDTYPE")
     """Вид облигации"""
 
-    bond_sub_type: str | None = Field(None, alias="BONDSUBTYPE")
+    bond_sub_type: str | None = Field(default=None, alias="BONDSUBTYPE")
     """Подвид облигации"""
 
     # --- Параметры номинала ---
-    face_value: MoexDecimal = Field(None, alias="FACEVALUE")
+    face_value: MoexDecimal = Field(default=None, alias="FACEVALUE")
     """Непогашенный долг"""
 
-    face_unit: str | None = Field(None, alias="FACEUNIT")
+    face_unit: str | None = Field(default=None, alias="FACEUNIT")
     """Валюта номинала"""
 
-    currency_id: str | None = Field(None, alias="CURRENCYID")
+    currency_id: str | None = Field(default=None, alias="CURRENCYID")
     """Валюта, в которой проводятся расчеты по сделкам"""
 
-    lot_size: MoexInt = Field(None, alias="LOTSIZE")
+    lot_size: MoexInt = Field(default=None, alias="LOTSIZE")
     """Размер лота"""
 
-    lot_value: MoexDecimal = Field(None, alias="LOTVALUE")
+    lot_value: MoexDecimal = Field(default=None, alias="LOTVALUE")
     """Номинальная стоимость лота, в валюте номинала"""
 
-    min_step: MoexDecimal = Field(None, alias="MINSTEP")
+    min_step: MoexDecimal = Field(default=None, alias="MINSTEP")
     """Мин. шаг цены"""
 
-    issue_size: MoexInt = Field(None, alias="ISSUESIZE")
+    issue_size: MoexInt = Field(default=None, alias="ISSUESIZE")
     """Объем выпуска, штук ценных бумаг"""
 
-    issue_size_placed: MoexInt = Field(None, alias="ISSUESIZEPLACED")
+    issue_size_placed: MoexInt = Field(default=None, alias="ISSUESIZEPLACED")
     """Количество ценных бумаг в обращении"""
 
     # --- Купоны и доходность ---
-    coupon_value: MoexDecimal = Field(None, alias="COUPONVALUE")
+    coupon_value: MoexDecimal = Field(default=None, alias="COUPONVALUE")
     """Сумма купона, в валюте номинала"""
 
-    coupon_percent: MoexDecimal = Field(None, alias="COUPONPERCENT")
+    coupon_percent: MoexDecimal = Field(default=None, alias="COUPONPERCENT")
     """Ставка купона, %"""
 
-    accruedint: MoexDecimal = Field(None, alias="ACCRUEDINT")
+    accruedint: MoexDecimal = Field(default=None, alias="ACCRUEDINT")
     """НКД на дату расчетов, в валюте расчетов"""
 
-    next_coupon: MoexDate = Field(None, alias="NEXTCOUPON")
+    next_coupon: MoexDate = Field(default=None, alias="NEXTCOUPON")
     """Дата окончания купона"""
 
-    coupon_period: MoexInt = Field(None, alias="COUPONPERIOD")
+    coupon_period: MoexInt = Field(default=None, alias="COUPONPERIOD")
     """Длительность купона"""
 
     # --- Календарь и цены ---
-    mat_date: MoexDate = Field(None, alias="MATDATE")
+    mat_date: MoexDate = Field(default=None, alias="MATDATE")
     """Дата погашения"""
 
-    buyback_date: MoexDate = Field(None, alias="BUYBACKDATE")
+    buyback_date: MoexDate = Field(default=None, alias="BUYBACKDATE")
     """Дата, к кот.рассч.доходность"""
 
-    buyback_price: MoexDecimal = Field(None, alias="BUYBACKPRICE")
+    buyback_price: MoexDecimal = Field(default=None, alias="BUYBACKPRICE")
     """Цена оферты"""
 
-    offer_date: MoexDate = Field(None, alias="OFFERDATE")
+    offer_date: MoexDate = Field(default=None, alias="OFFERDATE")
     """Дата Оферты"""
 
-    prev: MoexDecimal = Field(None, alias="PREV")
+    prev: MoexDecimal = Field(default=None, alias="PREV")
     """Цена последней сделки предыдущего торгового дня, % от номинала"""
 
-    prev_price: MoexDecimal = Field(None, alias="PREVPRICE")
+    prev_price: MoexDecimal = Field(default=None, alias="PREVPRICE")
     """Цена последней сделки пред. дня, % к номиналу"""
 
-    prev_weighted_price: MoexDecimal = Field(None, alias="PREVWAPRICE")
+    prev_weighted_price: MoexDecimal = Field(default=None, alias="PREVWAPRICE")
     """Средневзвешенная цена предыдущего дня, % к номиналу"""
 
-    yield_at_prev_weighted_price: MoexDecimal = Field(None, alias="YIELDATPREVWAPRICE")
+    yield_at_prev_weighted_price: MoexDecimal = Field(
+        default=None, alias="YIELDATPREVWAPRICE"
+    )
     """Доходность по оценке пред. дня"""
 
-    decimals_char: MoexDecimal = Field(None, alias="DECIMALS")
+    decimals_char: MoexDecimal = Field(default=None, alias="DECIMALS")
     """Точность, знаков после запятой"""
 
     # --- Цены (% от номинала) ---
-    price_percent: MoexDecimal = Field(None, alias="LAST")
+    price_percent: MoexDecimal = Field(default=None, alias="LAST")
     """Цена последней сделки, %"""
 
-    open_price: MoexDecimal = Field(None, alias="OPEN")
+    open_price: MoexDecimal = Field(default=None, alias="OPEN")
     """Цена первой сделки, % к номиналу"""
 
-    close_price: MoexDecimal = Field(None, alias="CLOSE")
+    close_price: MoexDecimal = Field(default=None, alias="CLOSE")
     """Цена последней сделки, %"""
 
-    low_price: MoexDecimal = Field(None, alias="LOW")
+    low_price: MoexDecimal = Field(default=None, alias="LOW")
     """Минимальная цена сделки, % к номиналу"""
 
-    high_price: MoexDecimal = Field(None, alias="HIGH")
+    high_price: MoexDecimal = Field(default=None, alias="HIGH")
     """Максимальная цена сделки, % к номиналу"""
 
-    weighted_price: MoexDecimal = Field(None, alias="WAPRICE")
+    weighted_price: MoexDecimal = Field(default=None, alias="WAPRICE")
     """Средневзвешенная цена, % к номиналу"""
 
-    lclose_price: MoexDecimal = Field(None, alias="CLOSEPRICE")
+    lclose_price: MoexDecimal = Field(default=None, alias="CLOSEPRICE")
     """Цена закрытия"""
 
-    prev_close_price: MoexDecimal = Field(None, alias="PREVLEGALCLOSEPRICE")
+    prev_close_price: MoexDecimal = Field(default=None, alias="PREVLEGALCLOSEPRICE")
     """Официальная цена закрытия предыдущего дня"""
 
     # --- Спрос и предложение ---
-    bid: MoexDecimal = Field(None, alias="BID")
-    """Цена спроса (котировка на покупку) на момент окончания торговой сессии, % от номинала"""
+    bid: MoexDecimal = Field(default=None, alias="BID")
+    """
+    Цена спроса (котировка на покупку)
+    на момент окончания торговой сессии, % от номинала
+    """
 
-    offer: MoexDecimal = Field(None, alias="OFFER")
-    """Цена предложения (котировка на продажу) на момент окончания торговой сессии, % от номинала"""
+    offer: MoexDecimal = Field(default=None, alias="OFFER")
+    """
+    Цена предложения (котировка на продажу) 
+    на момент окончания торговой сессии, % от номинала
+    """
 
-    spread: MoexDecimal = Field(None, alias="SPREAD")
+    spread: MoexDecimal = Field(default=None, alias="SPREAD")
     """Разница между лучшей котировкой на продажу и покупку (спред), % к номиналу"""
 
-    bid_deptht: MoexInt = Field(None, alias="BIDDEPTHT")
+    bid_deptht: MoexInt = Field(default=None, alias="BIDDEPTHT")
     """Совокупный спрос"""
 
-    offer_deptht: MoexInt = Field(None, alias="OFFERDEPTHT")
+    offer_deptht: MoexInt = Field(default=None, alias="OFFERDEPTHT")
     """Общий объем котировок на продажу, лотов"""
 
     # --- Активность и объемы ---
 
-    num_trades: MoexInt = Field(None, alias="NUMTRADES")
+    num_trades: MoexInt = Field(default=None, alias="NUMTRADES")
     """Количество заключенных сделок, штук"""
 
-    volume_today: MoexInt = Field(None, alias="VOLTODAY")
+    volume_today: MoexInt = Field(default=None, alias="VOLTODAY")
     """Объем заключенных сделок в единицах ценных бумаг, штук"""
 
-    value_today: MoexDecimal = Field(None, alias="VALTODAY")
+    value_today: MoexDecimal = Field(default=None, alias="VALTODAY")
     """Объем в валюте, в которой проводятся расчеты по сделкам"""
 
-    qty: MoexInt = Field(None, alias="QTY")
+    qty: MoexInt = Field(default=None, alias="QTY")
     """Объем последней сделки, лотов"""
 
-    value: MoexDecimal = Field(None, alias="VALUE")
+    value: MoexDecimal = Field(default=None, alias="VALUE")
     """Объем последней сделки, руб."""
 
     # --- Расчетные показатели ---
 
-    last_yield: MoexDecimal = Field(None, alias="YIELD")
+    last_yield: MoexDecimal = Field(default=None, alias="YIELD")
     """Доходность по последней сделке"""
 
-    effective_yield: MoexDecimal = Field(None, alias="EFFECTIVEYIELD")
+    effective_yield: MoexDecimal = Field(default=None, alias="EFFECTIVEYIELD")
     """Эффективная доходность"""
 
-    yield_at_weighted_price: MoexDecimal = Field(None, alias="YIELDATWAPRICE")
+    yield_at_weighted_price: MoexDecimal = Field(default=None, alias="YIELDATWAPRICE")
     """Доходность по средневзвешенной цене, % годовых"""
 
-    duration: MoexInt = Field(None, alias="DURATION")
+    duration: MoexInt = Field(default=None, alias="DURATION")
     """Дюрация, дней"""
 
-    last_change: MoexInt = Field(None, alias="LASTCHANGE")
-    """Изменение цены последней сделки к цене последней сделки предыдущего торгового дня"""
+    last_change: MoexInt = Field(default=None, alias="LASTCHANGE")
+    """
+    Изменение цены последней сделки
+    к цене последней сделки предыдущего торгового дня
+    """
 
     # --- Служебная информация ---
-    trading_status: str | None = Field(None, alias="TRADINGSTATUS")
+    trading_status: str | None = Field(default=None, alias="TRADINGSTATUS")
     """Состояние торговой сессии"""
 
     # --- Computed ---
@@ -211,23 +223,23 @@ class Bond(BaseInstrument):
         if self.price_percent is None or self.face_value is None:
             return None
 
-        return self.face_value * self.price_percent / Decimal(100)
+        return self.face_value * self.price_percent / Decimal(value=100)
 
     @computed_field
     @property
     def last_dirty_price(self) -> Decimal | None:
         """Грязная цена (Clean Price + НКД)."""
-        clean = self.last_price
+        clean: Decimal | None = self.last_price
 
         if clean is None:
             return None
 
-        return clean + (self.accruedint or Decimal(0))
+        return clean + (self.accruedint or Decimal(value=0))
 
     # --- Validator ---
     @model_validator(mode="before")
     @classmethod
-    def fix_missing_prices(cls, data: dict):
+    def fix_missing_prices(cls, data: dict[str, object]) -> dict[str, object]:
         """
         Если нет цены сделки (LAST), ищем цену закрытия или предыдущего дня.
         """
@@ -241,9 +253,10 @@ class Bond(BaseInstrument):
         return data
 
     # --- Repr ---
+    @override
     def __repr__(self) -> str:
         """Короткое человекочитаемое представление облигации."""
-        parts = [self.sec_id]
+        parts: list[str] = [self.sec_id]
 
         if self.short_name:
             parts.append(self.short_name)
@@ -255,6 +268,11 @@ class Bond(BaseInstrument):
             parts.append(f"yield={self.effective_yield:.2f}%")
 
         return f"<Bond {' | '.join(parts)}>"
+
+    # --- Str ---
+    @override
+    def __str__(self) -> str:
+        return repr(self)
 
 
 __all__ = ["Bond"]
