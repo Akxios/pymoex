97d60a6b91759a4f3d7a70e175cd102f6adc541e
Axkios <artemermolaev466@gmail.com>
2026-05-09 15:51:12 +0900

    Update: examples async usage

--- examples/async_usage.py
+++ examples/async_usage.py
@@ -1,47 +1,55 @@
 import asyncio
 
 from pymoex import MoexClient
+from pymoex.models.bond import Bond
+from pymoex.models.bondization import Amortization, Coupon
+from pymoex.models.search import Search
 
 
-async def main():
+async def main() -> None:
     async with MoexClient() as client:
         # --- 1. РЫНОК АКЦИЙ ---
         share_ticker = "SBER"
-        share = await client.share(share_ticker)
+        share = await client.share(ticker=share_ticker)
 
         print("--- Акции ---")
         print(f"Название: {share.short_name}")
         print(f"Текущая цена: {share.prev_price} {share.currency_id}")
 
         print("\nПоследние дивиденды:")
-        dividends = await client.dividends(share_ticker)
+        dividends = await client.dividends(ticker=share_ticker)
         if dividends:
             for div in dividends[-5:]:  # Берем 5 самых свежих
-                print(
-                    f" - Отсечка {div.registry_close_date}: Выплата {div.value} {div.currency_id}"
+                message: str = (
+                    f" - Отсечка {div.registry_close_date}: "
+                    f"Выплата {div.value} {div.currency_id}"
                 )
+                print(message)
         else:
             print("Дивиденды не найдены.")
 
         # --- 2. РЫНОК ОБЛИГАЦИЙ ---
         bond_ticker = "RU000A10DS74"
-        bond = await client.bond(bond_ticker)
+        bond: Bond = await client.bond(ticker=bond_ticker)
 
         print("\n--- Облигации ---")
         print(f"Название: {bond.short_name}")
         print(f"Текущая цена: {bond.price_percent}%")
         print(f"Доходность: {bond.effective_yield}%")
         print(
-            f"Ближайший купон: {bond.next_coupon} ({bond.coupon_value} {bond.face_unit})"
+            f"Ближайший купон: {bond.next_coupon} ",
+            f"({bond.coupon_value} {bond.face_unit})",
         )
 
         print("\nГрафик купонов:")
-        coupons = await client.coupons(bond_ticker)
+        coupons: list[Coupon] = await client.coupons(ticker=bond_ticker)
         for coupon in coupons[:3]:  # Выводим только первые 3
             print(f" - {coupon.coupon_date}: {coupon.value} {coupon.face_unit}")
 
         print("\nГрафик амортизации:")
-        amortizations = await client.amortizations(bond_ticker)
+        amortizations: list[Amortization] = await client.amortizations(
+            ticker=bond_ticker
+        )
         if amortizations:
             for amort in amortizations:
                 print(
@@ -53,11 +61,11 @@ async def main():
         # --- 3. ГЛОБАЛЬНЫЙ ПОИСК ---
         query = "Сбербанк"
         print(f"\n--- Поиск: {query} ---")
-        results = await client.find(query, instrument_type="share")
+        results: list[Search] = await client.find(query, instrument_type="share")
 
         for r in results[:3]:  # Показываем только Топ-3 результата
             print(f" - {r.name} ({r.sec_id}) | Рег. номер: {r.reg_number}")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main=main())
