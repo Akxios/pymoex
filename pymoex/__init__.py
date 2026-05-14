cb757546f456ae65b99952183fc53e6f32933ac2
Axkios <artemermolaev466@gmail.com>
2026-05-14 20:28:10 +0900

    Update: __init__.py

--- pymoex/__init__.py
+++ pymoex/__init__.py
@@ -1,6 +1,4 @@
-"""pymoex: Асинхронный SDK для Московской биржи (MOEX) API."""
-
-__version__ = "0.1.6"
+"""pymoex: асинхронный SDK для Московской биржи (MOEX ISS API)."""
 
 import logging
 
@@ -20,20 +18,22 @@ from .api import (
 )
 from .client import MoexClient
 
-logging.getLogger(name=__name__).addHandler(hdlr=logging.NullHandler())
+__version__ = "0.1.6"
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = [
     "MoexClient",
-    "get_share",
-    "get_bond",
-    "get_coupons",
-    "get_dividends",
-    "get_amortizations",
     "find",
-    "find_shares",
     "find_bonds",
     "find_currencies",
     "find_funds",
+    "find_shares",
+    "get_amortizations",
+    "get_bond",
+    "get_coupons",
     "get_currency",
+    "get_dividends",
     "get_fund",
+    "get_share",
 ]
